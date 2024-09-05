66429754c83982828ac4db21f497bee52ee2279a
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-09-05 16:30:11 -0600

    refactor: Move core logic to a `FileStream`

--- tap_csv/client.py
+++ tap_csv/client.py
@@ -3,34 +3,16 @@
 from __future__ import annotations
 
 import csv
-import os
 import typing as t
-from functools import cached_property
 
-import fsspec
-from singer_sdk import typing as th
-from singer_sdk.streams import Stream
+from .file_stream import FileStream
 
 if t.TYPE_CHECKING:
     from singer_sdk.helpers.types import Context
 
-SDC_SOURCE_FILE_COLUMN = "_sdc_source_file"
-SDC_SOURCE_LINENO_COLUMN = "_sdc_source_lineno"
-SDC_SOURCE_FILE_MTIME_COLUMN = "_sdc_source_file_mtime"
 
-
-class CSVStream(Stream):
+class CSVStream(FileStream):
     """Stream class for CSV streams."""
-
-    file_paths: list[str] = []  # noqa: RUF012
-    header: list[str] = []  # noqa: RUF012
-
-    def __init__(self, filesystem: str, *args, options: dict[str, t.Any], **kwargs):
-        """Init CSVStram."""
-        # cache file_config so we dont need to go iterating the config list again later
-        self.file_config = kwargs.pop("file_config")
-        self.fs = fsspec.filesystem(filesystem, **options)
-        super().__init__(*args, **kwargs)
 
     def get_records(self, context: Context | None) -> t.Iterable[dict]:
         """Return a generator of row-type dictionary objects.
@@ -63,47 +45,6 @@ class CSVStream(Stream):
 
                 yield dict(zip(self.header, row))
 
-    def _get_recursive_file_paths(self, file_path: str) -> list:
-        file_paths = []
-
-        for dirpath, _, filenames in self.fs.walk(file_path):
-            for filename in filenames:
-                file_path = os.path.join(dirpath, filename)
-                if self.is_valid_filename(file_path):
-                    file_paths.append(file_path)
-
-        return file_paths
-
-    def get_file_paths(self) -> list:
-        """Return a list of file paths to read.
-
-        This tap accepts file names and directories so it will detect
-        directories and iterate files inside.
-        """
-        # Cache file paths so we dont have to iterate multiple times
-        if self.file_paths:
-            return self.file_paths
-
-        file_path = self.file_config["path"]
-        if not self.fs.exists(file_path):
-            raise Exception(f"File path does not exist {file_path}")
-
-        file_paths = []
-        if self.fs.isdir(file_path):
-            clean_file_path = os.path.normpath(file_path) + os.sep
-            file_paths = self._get_recursive_file_paths(clean_file_path)
-
-        elif self.is_valid_filename(file_path):
-            file_paths.append(file_path)
-
-        if not file_paths:
-            raise RuntimeError(
-                f"Stream '{self.name}' has no acceptable files. \
-                    See warning for more detail."
-            )
-        self.file_paths = file_paths
-        return file_paths
-
     def is_valid_filename(self, file_path: str) -> bool:
         """Return a boolean of whether the file includes CSV extension."""
         is_valid = True
@@ -129,40 +70,3 @@ class CSVStream(Stream):
         )
         with self.fs.open(file_path, mode="r", encoding=encoding) as f:
             yield from csv.reader(f, dialect="tap_dialect")
-
-    @cached_property
-    def schema(self) -> dict:
-        """Return dictionary of record schema.
-
-        Dynamically detect the json schema for the stream.
-        This is evaluated prior to any records being retrieved.
-        """
-        properties: list[th.Property] = []
-        self.primary_keys = self.file_config.get("keys", [])
-
-        for file_path in self.get_file_paths():
-            for header in self.get_rows(file_path):  # noqa: B007
-                break
-            break
-
-        properties.extend(th.Property(column, th.StringType()) for column in header)
-        # If enabled, add file's metadata to output
-        if self.config.get("add_metadata_columns", False):
-            header = [
-                SDC_SOURCE_FILE_COLUMN,
-                SDC_SOURCE_FILE_MTIME_COLUMN,
-                SDC_SOURCE_LINENO_COLUMN,
-                *header,
-            ]
-
-            properties.extend(
-                (
-                    th.Property(SDC_SOURCE_FILE_COLUMN, th.StringType),
-                    th.Property(SDC_SOURCE_FILE_MTIME_COLUMN, th.DateTimeType),
-                    th.Property(SDC_SOURCE_LINENO_COLUMN, th.IntegerType),
-                )
-            )
-        # Cache header for future use
-        self.header = header
-
-        return th.PropertiesList(*properties).to_dict()
